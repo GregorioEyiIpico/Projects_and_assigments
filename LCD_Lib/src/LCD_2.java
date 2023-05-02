27eb590bb580a27bd522d357f1cd730e0e266adb
Gregorio Eyi <gregorioeyi@gmail.com>
2023-05-02 15:07:12 +0700

    Creating a new project name LDClib

---
+++ LCD_Lib/src/LCD_2.java
@@ -0,0 +1,103 @@
+public class LCD_2 {
+
+    public static void clearScreen() {
+        for (int i = 0; i<25; i++){
+            System.out.println(" ");
+        }
+    }
+
+    public static void showMainMenu() {
+        System.out.println("|--------------------------------------|");
+        System.out.println("| Welcome to Stamford LCD Library Demo |");
+        System.out.println("|                                      |");
+        System.out.println("|       Select a Function (1-3)        |");
+        System.out.println("|--------------------------------------|");
+        System.out.println("|                                      |");
+        System.out.println("|   1. Login                           |");
+        System.out.println("|   2. Restart                         |");
+        System.out.println("|   3. Shutdown                        |");
+        System.out.println("|                                      |");
+        System.out.println("|--------------------------------------|");
+    }
+
+    public static void showLoginMenu() {
+        System.out.println("----------------------------------------------");
+        System.out.println("| Login Menu. Press F1 for more information. |");
+        System.out.println("----------------------------------------------");
+        System.out.println("|                                            |");
+        System.out.println("|               Enter Username:              |");
+        System.out.println("|                                            |");
+        System.out.println("|--------------------------------------------|");
+        //userName();
+    }
+
+    public static void showSystemMenu() {
+        System.out.println("----------------------------------------------");
+        System.out.println("|          Select a Function (1-4).          |");
+        System.out.println("----------------------------------------------");
+        System.out.println("|                                            |");
+        System.out.println("|    1. Change AC Temperature set point      |");
+        System.out.println("|    2. Enable/Disable IP Camera             |");
+        System.out.println("|    3. Turn anti-theft system on/off.       |");
+        System.out.println("|    4. Exit                                 |");
+        System.out.println("|                                            |");
+        System.out.println("|--------------------------------------------|");
+    }
+
+    public static void changeACSetP() {
+        System.out.println("----------------------------------------------");
+        System.out.println("|     Applying New Temperature Set Point.    |");
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|                                            |");
+        System.out.println("|     New temperature set point will be in   |");
+        System.out.println("|                effect soon...              |");
+        System.out.println("|                                            |");
+        System.out.println("|--------------------------------------------|");
+    }
+
+    public static void confirmRestart() {
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|            Restart Confirmation            |");
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|                                            |");
+        System.out.println("|    Are you sure that you want to restart   |");
+        System.out.println("|             the system? (Y/N)              |");
+        System.out.println("|                                            |");
+        System.out.println("|--------------------------------------------|");
+    }
+
+    public static void confirmShutdown() {
+        System.out.println("----------------------------------------------");
+        System.out.println("|           Shutdown Confirmation            |");
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|                                            |");
+        System.out.println("|   Are you sure that you want to shutdown   |");
+        System.out.println("|              the system? (Y/N)             |");
+        System.out.println("|                                            |");
+        System.out.println("|--------------------------------------------|");
+    }
+
+    public static void showRestart() {
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|                 Restarting ....            |");
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|                                            |");
+        System.out.println("|       Please wait while the system is      |");
+        System.out.println("|           preparing for a restart.         |");
+        System.out.println("|                                            |");
+        System.out.println("|--------------------------------------------|");
+    }
+
+    public static void showShutdown() {
+
+        System.out.println("|-----------------------------------------|");
+        System.out.println("|             Shutting down ....          |");
+        System.out.println("|-----------------------------------------|");
+        System.out.println("|                                         |");
+        System.out.println("|    Please wait while the system is      |");
+        System.out.println("|        preparing for a shutdown.        |");
+        System.out.println("|                                         |");
+        System.out.println("|-----------------------------------------|");
+    }
+
+}
---
+++ LCD_Lib/src/MyIoTProject.java
@@ -0,0 +1,12 @@
+// Week 1 exercice
+public class MyIoTProject {
+    public static void main(String[] args) {
+        LCD_2.showMainMenu();
+        LCD_2.showLoginMenu();
+        LCD_2.showSystemMenu();
+        LCD_2.changeACSetP();
+        LCD_2.confirmRestart();
+        LCD_2.confirmShutdown();
+        LCD_2.showShutdown();
+    }
+}
