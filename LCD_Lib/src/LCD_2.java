d8411ac46e4c4ee6e0beeda002b0bcf35d21f115
Gregorio Eyi <gregorioeyi@gmail.com>
2023-05-02 16:05:44 +0700

    Modifying LDC_lib in LDC_2.java class

--- LCD_Lib/src/LCD_2.java
+++ LCD_Lib/src/LCD_2.java
@@ -1,4 +1,15 @@
 public class LCD_2 {
+    int x = 8;
+    String s = "hello there";
+    int stringLength = s.length();
+    final static char escCode = 0x1B;
+    final static char topLeft = 9484;
+    final static char topRight = 9488;
+    final static char bottomLeft = 9492;
+    final static char bottomRight = 9496;
+    final static char dash = '-';
+    final static char midLeft = 9500;
+    final static char midRight = 9508;
 
     public static void clearScreen() {
         for (int i = 0; i<25; i++){
@@ -100,4 +111,64 @@ public class LCD_2 {
         System.out.println("|-----------------------------------------|");
     }
 
+    public static void printBoxLine(String text, int alignment) {
+        int textLength = text.length();
+        int padding;
+        boolean isEven;
+        if (textLength % 2 == 0) {
+            padding = (80 - (6 + textLength)) / 2;
+            isEven = true;
+            System.out.println("Padding value: " + padding + "\n" + "Is even?: "+ isEven);
+        } else {
+            padding = (80 - (6 + textLength + 1)) / 2;
+            isEven = false;
+            System.out.println("Padding value: " + padding + "\n" + "Is even?: "+ isEven);
+        }
+
+
+        System.out.println("how many space:"+ (75 - textLength));
+        switch (alignment) {
+            case 1:
+                // alignment right
+                System.out.print("| ");
+                printSpace(75 - textLength);
+                System.out.print(text);
+                System.out.print(" |");
+
+                break;
+            case 2:
+                // alignment center
+                if (isEven) {
+                    System.out.println("Entering here bc it's even");
+                    System.out.print("| ");
+                    printSpace(padding);
+                    System.out.print(text);
+                    printSpace(padding);
+                    System.out.print(" |");
+                    break;
+                } else {
+                    System.out.println("Entering here bc it's not even");
+                    System.out.print("| ");
+                    printSpace(padding);
+                    System.out.print(text);
+                    printSpace(padding + 1);
+                    System.out.print(" |");
+                    break;
+                }
+            default:
+                // 0:alignment left
+                System.out.println("Entering here bc it's the default");
+                System.out.print("| ");
+                System.out.print(text);
+                printSpace(75 - textLength);
+                System.out.print(" |");
+                break;
+        }
+    }
+
+    private static void printSpace(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("");
+        }
+    }
 }
--- LCD_Lib/src/MyIoTProject.java
+++ LCD_Lib/src/MyIoTProject.java
@@ -1,12 +1,13 @@
 // Week 1 exercice
 public class MyIoTProject {
     public static void main(String[] args) {
-        LCD_2.showMainMenu();
+        /*LCD_2.showMainMenu();
         LCD_2.showLoginMenu();
         LCD_2.showSystemMenu();
         LCD_2.changeACSetP();
         LCD_2.confirmRestart();
         LCD_2.confirmShutdown();
-        LCD_2.showShutdown();
+        LCD_2.showShutdown();*/
+        LCD_2.printBoxLine("Welcome to Stamford LCD Library Demo", 2);
     }
 }
